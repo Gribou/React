c36f5527d0b3358d3c9de20cda161d19e86858a4
Gribou <toutain@numericable.com>
2022-11-07 15:42:22 +0100

    Update index.js

--- src/src/components/index.js
+++ src/src/components/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/Root";
-import store_config from "store";
+import store_config from "./store";
 import configureLocale from "configureLocale";
 
 configureLocale();
